a43109c09229b9d1d0119213bc098c71b639929f
VoltBw <154667794+VoltBw@users.noreply.github.com>
2024-04-07 15:27:59 +0200

    V2

--- AdministrationVMs.ps1
+++ AdministrationVMs.ps1
@@ -1,6 +1,12 @@
 ﻿get-vm | where state -eq running
 get-vm | where state -eq running | select name
+get-vm | where state -eq running | Stop-VM 
+get-vm | where state -eq running | Stop-VM -force
 
 get-vm *hv* | Start-VM
 get-vm *hv* | Stop-VM -force
 
+Enter-PSSession -VMName LSRVHV01
+
+Checkpoint-VM "WHV01", "WHV02"
+*
