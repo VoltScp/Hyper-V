51e45c3d1eff9d60aad12874c624a3b167872906
VoltBw <154667794+VoltBw@users.noreply.github.com>
2024-04-09 11:22:19 +0200

    Update ConvergedNetwork.ps1

--- ConvergedNetwork.ps1
+++ ConvergedNetwork.ps1
@@ -1,20 +1,43 @@
-﻿New-VMSwitch -Name SwitchSet -NetAdapterName "Lan 1","Lan 2" -EnableEmbeddedTeaming $true -AllowManagementOS $false 
+New-VMSwitch -Name SwitchSet -NetAdapterName "Lan 1","Lan 2" -EnableEmbeddedTeaming $true -AllowManagementOS $false -MinimumBandwidthMode Weight
+Set-VMSwitchTeam -Name SwitchSet -LoadBalancingAlgorithm Dynamic
 
 Get-VMSwitch
 Get-VMSwitch |  fl
+Get-VMSwitch -SwitchName $Vswitch | Select-Object BandwidthReservationMode
 Get-VMSwitchTeam -SwitchName $Vswitch | Format-List
 
-Set-VMSwitchTeam -Name SwitchSet -LoadBalancingAlgorithm Dynamic
-
 Add-VMNetworkAdapter -ManagementOS -SwitchName SwitchSet -Name Management
-Add-VMNetworkAdapter -ManagementOS -SwitchName SwitchSet -Name Heartbeat
+Add-VMNetworkAdapter -ManagementOS -SwitchName SwitchSet -Name Cluster
 Add-VMNetworkAdapter -ManagementOS -SwitchName SwitchSet -Name LiveMigration
+Add-VMNetworkAdapter -ManagementOS -SwitchName SwitchSet -Name Backup
 Add-VMNetworkAdapter -ManagementOS -SwitchName SwitchSet -Name Storage-1
 Add-VMNetworkAdapter -ManagementOS -SwitchName SwitchSet -Name Storage-2
 
+Get-VMNetworkAdapter -ManagementOS
+
 Set-VMNetworkAdapterVlan -ManagementOS -VMNetworkAdapterName Management -Access -VlanId 10
-Set-VMNetworkAdapterVlan -ManagementOS -VMNetworkAdapterName Storage-1 -Access -VlanId 11
-Set-VMNetworkAdapterVlan -ManagementOS -VMNetworkAdapterName Storage-2 -Access -VlanId 11
+Set-VMNetworkAdapterVlan -ManagementOS -VMNetworkAdapterName Cluster -Access -VlanId 11
+Set-VMNetworkAdapterVlan -ManagementOS -VMNetworkAdapterName LiveMigration -Access -VlanId 12
+Set-VMNetworkAdapterVlan -ManagementOS -VMNetworkAdapterName Backup -Access -VlanId 13
+
+
+(Get-VMNetworkAdapter -ManagementOS -Name "Management").VlanSetting
+(Get-VMNetworkAdapter -ManagementOS -Name "Cluster").VlanSetting
+(Get-VMNetworkAdapter -ManagementOS -Name "LiveMigration").VlanSetting
+(Get-VMNetworkAdapter -ManagementOS -Name "Backup").VlanSetting
+
+Get-VMNetworkAdapterVlan -ManagementOS
+
+Get-VMNetworkAdapter -ManagementOS -Name "MGMT" | Set-VMNetworkAdapter -MinimumBandwidthWeight 10
+Get-VMNetworkAdapter -ManagementOS -Name "Cluster" | Set-VMNetworkAdapter -MinimumBandwidthWeight 10
+Get-VMNetworkAdapter -ManagementOS -Name "LiveMigration" | Set-VMNetworkAdapter -MinimumBandwidthWeight 40
+Get-VMNetworkAdapter -ManagementOS -Name "Backup" | Set-VMNetworkAdapter -MinimumBandwidthWeight 40
+
+(Get-VMNetworkAdapter -ManagementOS -Name "MGMT").BandwidthSetting
+(Get-VMNetworkAdapter -ManagementOS -Name "Cluster").BandwidthSetting
+(Get-VMNetworkAdapter -ManagementOS -Name "LiveMigration").BandwidthSetting
+(Get-VMNetworkAdapter -ManagementOS -Name "Backup").BandwidthSetting
+
 
 Enable-NetAdapterRdma -Name "vEthernet (Storage-1)","vEthernet (Storage-2)"
 Get-NetAdapterRdma |? Enabled -like $true
